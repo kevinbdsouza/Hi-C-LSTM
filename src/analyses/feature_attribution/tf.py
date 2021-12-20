6d63a0fe41842b92e56981e82cdbae9fc5daf504
kevinbdsouza <kdsouza1496@gmail.com>
2021-12-20 11:01:09 -0800

    fi

--- src/analyses/feature_attribution/tf.py
+++ src/analyses/feature_attribution/tf.py
@@ -1,11 +1,6 @@
 import pandas as pd
-import re
 import os
-import numpy as np
-from train_fns import config
-from train_fns.data_prep_hic import DataPrepHic
-from downstream.downstream_helper import DownstreamHelper
-
+from training import config
 
 class TFChip:
     def __init__(self, cfg, cell, chr):
