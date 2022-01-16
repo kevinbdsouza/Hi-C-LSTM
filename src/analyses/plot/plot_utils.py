9bed346d647aee5c314d9293b6c36e23b5693ad7
kevinbdsouza <kdsouza1496@gmail.com>
2022-01-16 14:18:10 -0800

    ctcf dots

--- src/analyses/plot/plot_utils.py
+++ src/analyses/plot/plot_utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib as mpl
-mpl.use("Gtk3Agg")
+mpl.use("TkAgg")
 import training.config as config
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
