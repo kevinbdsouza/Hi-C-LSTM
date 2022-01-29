fd6494f24e7e3933c5de71a244de939ef1b14b4c
kevinbdsouza <kdsouza1496@gmail.com>
2022-01-29 11:32:49 -0800

    test

--- src/analyses/plot/plot_utils.py
+++ src/analyses/plot/plot_utils.py
@@ -196,13 +196,13 @@ if __name__ == '__main__':
         #foxg1_ko = np.load(cfg.output_directory + "foxg1_ko.npy")
         #simple_plot(foxg1_ko)
 
-        #foxg1_ko = np.load(cfg.output_directory + "tal1_wt.npy")
-        #simple_plot(foxg1_ko)
+        tal1_wt = np.load(cfg.output_directory + "tal1_wt.npy")
+        simple_plot(tal1_wt)
         #pred_data = pd.read_csv(cfg.output_directory + "%s_predictions_chr.csv" % (cell), sep="\t")
         #plot_tal1_lmo2(pred_data)
 
-        tal_ko = pd.read_csv(cfg.hic_path + cell +"/talko_tal_df.txt", sep="\t")
-        plot_heatmaps(tal_ko)
+        #tal_ko = pd.read_csv(cfg.hic_path + cell +"/talko_tal_df.txt", sep="\t")
+        #plot_heatmaps(tal_ko)
 
     print("done")
 
