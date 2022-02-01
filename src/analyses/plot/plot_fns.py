560d24c8dde00f3bddcf9f890467a71149e8fe69
kevinbdsouza <kdsouza1496@gmail.com>
2022-01-31 17:36:57 -0800

    test

--- src/analyses/plot/plot_fns.py
+++ src/analyses/plot/plot_fns.py
@@ -890,7 +890,7 @@ class PlotFns:
         plt.legend(loc='upper right', fontsize=20)
         '''
 
-        plt.savefig("/home/kevindsouza/Downloads/r2_gm.png")
+        plt.savefig("/home/kevindsouza/Downloads/r2_h1.png")
         plt.show()
 
         print("done")
@@ -1144,7 +1144,7 @@ if __name__ == "__main__":
     # plot_ob.plot_xgb()
     # plot_ob.plot_gbr()
 
-    plot_ob.plot_r2(cell = "GM12878")
+    plot_ob.plot_r2(cell = "H1hESC")
     # plot_ob.plot_r2_celltypes()
     # plot_ob.plot_symmetry()
 
