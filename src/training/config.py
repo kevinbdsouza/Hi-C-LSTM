2e8cd9ecd9cff427721323824e0d3408e3dd1394
kevinbdsouza <kdsouza1496@gmail.com>
2022-02-02 14:21:38 -0800

    test

--- src/training/config.py
+++ src/training/config.py
@@ -10,8 +10,8 @@ class Config:
         ##########################################
 
         self.num_chr = 23
-        self.genome_len = 1440455
-        self.resolution = 2000
+        self.genome_len = 28809
+        self.resolution = 100000
 
         ##########################################
         ############ Model Parameters ############
@@ -40,7 +40,7 @@ class Config:
         ############ Input Directories ###########
         ##########################################
 
-        self.cell = "GM12878_2kb"
+        self.cell = "GM12878_100kb"
         self.hic_path = '/data2/hic_lstm/data/'
         self.sizes_file = 'chr_cum_sizes2.npy'
         self.start_end_file = 'starts.npy'
