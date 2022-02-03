40e2a37580c47a3b65485971e49085a108a2d08f
kevinbdsouza <kdsouza1496@gmail.com>
2022-02-02 20:01:27 -0800

    test

--- src/training/config.py
+++ src/training/config.py
@@ -10,7 +10,7 @@ class Config:
         ##########################################
 
         self.num_chr = 23
-        self.genome_len = 5790
+        self.genome_len = 5762
         self.resolution = 500000
 
         ##########################################
