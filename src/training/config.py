44711f73d79310075ac456c0c8dc0707cdd150df
kevinbdsouza <kdsouza1496@gmail.com>
2022-02-25 11:12:10 -0800

    ig

--- src/training/config.py
+++ src/training/config.py
@@ -89,4 +89,4 @@ class Config:
         self.tf_columns = ["HGNC symbol", "chromosome", "start"]
         self.run_tfs = False
         self.run_elements = True
-        self.element = "ctcf"
+        self.element = "rad21"
