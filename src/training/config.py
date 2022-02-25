0d3f8529c2fb1ad470327b7a2b50f76ea5916d4c
kevinbdsouza <kdsouza1496@gmail.com>
2022-02-24 20:31:25 -0800

    ig

--- src/training/config.py
+++ src/training/config.py
@@ -55,7 +55,7 @@ class Config:
         self.dec_learning_rate = 0.01
         self.decoder_epochs = 20
         self.decoder_train_list = [21, 19]
-        self.decoder_test_list = [10, 11]
+        self.decoder_test_list = [5, 6, 7, 8, 9]
 
         "Output Directories"
         self.proj_dir = "/home/kevindsouza/Documents/projects/PhD/Hi-C-LSTM/"
