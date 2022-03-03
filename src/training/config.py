11fbafd98faa7b527fc281562f6230bbc9211817
kevinbdsouza <kdsouza1496@gmail.com>
2022-03-02 18:03:06 -0800

    ko

--- src/training/config.py
+++ src/training/config.py
@@ -131,7 +131,7 @@ class Config:
         self.ctcf_indices_22 = [284706, 284743]
         self.ko_window = 10
         self.ko_increment = 10
-        self.ko_mode = "average"
+        self.ko_mode = "zero"
         self.perform_ko = True
         self.load_ko = False
         self.normalize_embed = False
