ebb67a51f4266d09077aa6228255573512e01089
kevinbdsouza <kdsouza1496@gmail.com>
2022-06-05 09:46:19 -0700

    update

--- src/analyses/plot/plot_fns.py
+++ src/analyses/plot/plot_fns.py
@@ -114,7 +114,7 @@ class PlotFns:
             plt.subplots_adjust(bottom=0.35)
 
         if save:
-            plt.savefig("/home/kevindsouza/Downloads/acc_h1.svg", format="svg")
+            plt.savefig("/home/kevindsouza/Downloads/acc_hff.svg", format="svg")
 
         plt.show()
 
@@ -657,7 +657,7 @@ if __name__ == "__main__":
     plot_ob = PlotFns(cfg)
 
     "Chose among plotting functions"
-    plot_ob.plot_combined(cell="H1hESC", metric="accuracy", ylabel="Accuracy")
+    plot_ob.plot_combined(cell="HFFhTERT", metric="accuracy", ylabel="Accuracy")
     # plot_ob.plot_map_celltypes()
     # plot_ob.plot_map_resolutions()
     # plot_ob.plot_auroc_celltypes()
